920d154c4f0a57654253b38357a9b72a7d45a412
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-11-11 13:25:32 +0100

    Mise à jour du code kal3iya

--- kal3iya/models/advance.py
+++ kal3iya/models/advance.py
@@ -3,7 +3,6 @@ from odoo import models, fields, api
 class Kal3iyaAdvance(models.Model):
     _name = 'kal3iya.advance'
     _description = 'Avances'
-
     client_id = fields.Many2one('kal3iya.client', required=True)
     amount = fields.Float(string='Montant', required=True)
     date_paid = fields.Date(string='Date', required=True)

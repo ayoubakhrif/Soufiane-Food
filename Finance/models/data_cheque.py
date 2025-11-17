6125dc8aa095249c2572e2e3cdb2a34e01594262
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-11-17 17:38:22 +0100

    Mise à jour du code kal3iya

--- Finance/models/data_cheque.py
+++ Finance/models/data_cheque.py
@@ -20,7 +20,7 @@ class DataCheque(models.Model):
     facture = fields.Selection([
         ('m', 'M'),
         ('bureau', 'Bureau'),
-        ('facture', 'F/'),
+        ('fact', 'F/'),
     ], string='Facture', tracking=True, required=True, default='m')
     journal = fields.Integer(string='Journal N°', required=True)
     facture_tag = fields.Html(string='Facture', compute='_compute_facture_tag', sanitize=False)

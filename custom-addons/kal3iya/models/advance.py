dc25dc57af53b04bb551877faf9e5c6a06b40e0d
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-11-19 11:49:00 +0100

    Mise à jour

---
+++ custom-addons/kal3iya/models/advance.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class Kal3iyaAdvance(models.Model):
+    _name = 'kal3iya.advance'
+    _description = 'Avances'
+    client_id = fields.Many2one('kal3iya.client', required=True)
+    amount = fields.Float(string='Montant', required=True)
+    date_paid = fields.Date(string='Date', required=True)
+    driver_id = fields.Many2one('kal3iya.driver', string='Chauffeur')
+    comment = fields.Text(string='Commentaire')
+    payment_mode = fields.Selection([
+        ('chq', 'CHQ'),
+        ('espece', 'Espèce'),
+        ('virement', 'Virement'),
+        ('versement', 'Versement'),
+        ('charge', 'Charges'),
+    ], string='Mode de paiement', tracking=True)

d44445a417820ceccfd7bc8bc355f22d2076098c
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-11-20 18:43:33 +0100

    Mise à jour

--- custom-addons/finance/models/data_cheque.py
+++ custom-addons/finance/models/data_cheque.py
@@ -74,4 +74,18 @@ class DataCheque(models.Model):
     # ------------------------------------------------------------
     # CONTRAINTE D’UNICITÉ
     # ------------------------------------------------------------
-    
+    @api.onchange('chq')
+    def _onchange_chq_unique(self):
+        for rec in self:
+            if rec.chq:
+                domain = [('chq', '=', rec.chq)]
+
+                # 🔥 si le record existe déjà en base
+                if rec.id and isinstance(rec.id, int):
+                    domain.append(('id', '!=', rec.id))
+
+                # 🔍 Recherche
+                existing = self.env['datacheque'].search(domain, limit=1)
+
+                if existing:
+                    raise ValidationError("⚠️ Ce numéro de chèque existe déjà. Il doit être unique.")

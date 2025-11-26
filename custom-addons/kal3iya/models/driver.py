7411557b1a4d9e9926aa296e526c573f835472e2
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-11-26 15:25:35 +0100

    Mise à jour du code kal3iya

--- custom-addons/kal3iya/models/driver.py
+++ custom-addons/kal3iya/models/driver.py
@@ -7,3 +7,5 @@ class Kal3iyadriver(models.Model):
     name = fields.Char(string='Chauffeur', required=True)
     phone = fields.Char(string='Téléphone')
     advance_ids = fields.One2many('kal3iya.advance', 'driver_id', string='Avances')
+    total_avance = fields.float(string='Total_avances', compute='_compute_total_avance')
+    

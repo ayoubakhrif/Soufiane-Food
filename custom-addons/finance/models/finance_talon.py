f8853582f9fabdb3f77061fd2c604c01a4bc7fdc
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-12-10 17:08:11 +0100

    Mise à jour du code kal3iya

--- custom-addons/finance/models/finance_talon.py
+++ custom-addons/finance/models/finance_talon.py
@@ -3,8 +3,10 @@ from odoo import models, fields
 class FinanceSte(models.Model):
     _name = 'finance.talon'
     _description = 'Talons'
+    _rec_name = 'name_shown'
 
     name = fields.Char(string='Talon', required=True)
+    name_shown = fields.Char(string='Nom affiché', required=True)
     ste_id = fields.Many2one('finance.ste', string='Société', tracking=True, required=True)
     num_chq = fields.Integer(string='Nombres de chqs', required=True)
     serie = fields.Char(string='Série', required=True)

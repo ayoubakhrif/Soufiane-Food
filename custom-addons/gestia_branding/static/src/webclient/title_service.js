cc282468ffee05220e4827b742603c22dadc7a74
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-12-21 13:41:04 +0100

    Mise à jour du code kal3iya

--- custom-addons/gestia_branding/static/src/webclient/title_service.js
+++ custom-addons/gestia_branding/static/src/webclient/title_service.js
@@ -4,12 +4,14 @@ import { patch } from "@web/core/utils/patch";
 import { TitleService } from "@web/core/browser/title_service";
 
 patch(TitleService.prototype, {
+    /**
+     * Override setParts to replace Odoo with Gestia in browser title
+     */
     setParts(parts) {
-        // Call original behavior
-        this._super(parts);
-
-        // Replace Odoo branding in browser title
-        if (document.title && document.title.includes("Odoo")) {
+        // Call the original method
+        this._super(...arguments);
+        // Replace "Odoo" with "Gestia" in the document title
+        if (document.title.includes("Odoo")) {
             document.title = document.title.replace(/Odoo/g, "Gestia");
         }
     },

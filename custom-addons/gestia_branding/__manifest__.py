7655ab6841d8f7ba8aa4f7a3dcba409aafb70c6f
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-12-21 13:25:57 +0100

    Mise à jour du code kal3iya

---
+++ custom-addons/gestia_branding/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Gestia Branding',
+    'version': '17.0.1.0.0',
+    'category': 'Hidden',
+    'summary': 'Replace Odoo branding with Gestia in browser title',
+    'description': """
+        This module replaces "Odoo" with "Gestia" in the browser tab title.
+        No core file modifications - uses JavaScript override.
+    """,
+    'author': 'Ayoub Akhrif',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'gestia_branding/static/src/webclient/title_service.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}

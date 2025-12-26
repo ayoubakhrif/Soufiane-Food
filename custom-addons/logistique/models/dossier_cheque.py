7c0722a840a3164b45ef6e2526b914bb89780a3a
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2025-12-26 01:03:11 +0100

    Mise à jour du code kal3iya

--- custom-addons/logistique/models/dossier_cheque.py
+++ custom-addons/logistique/models/dossier_cheque.py
@@ -5,12 +5,12 @@ class LogistiqueDossierCheque(models.Model):
     _description = 'Chèque Dossier Logistique'
 
     dossier_id = fields.Many2one('logistique.dossier', string='Dossier', required=True, ondelete='cascade')
-    cheque_serie = fields.Char(string='Série Chèque', required=True)
+    cheque_serie = fields.Char(string='Série Chèque', required=True, size=7)
     date = fields.Date(string='Date')
     beneficiary_id = fields.Many2one('logistique.shipping', string='Bénéficiaire')
     amount = fields.Monetary(string='Montant', required=True, currency_field='currency_id')
-    currency_id = fields.Many2one('res.currency', string='Devise', required=True, default=lambda self: self.env.company.currency_id)
-    ste_id = fields.Many2one('res.company', string='Société', required=True, default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', string='Devise', required=True, default=lambda self: self.env.ref('base.MAD'))
+    ste_id = fields.Many2one('logistique.ste', string='Société', required=True)
     type = fields.Selection([
         ('thc', 'THC'),
         ('magasinage', 'Magasinage'),

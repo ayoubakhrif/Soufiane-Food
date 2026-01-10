7182d4ef4e041b0026c6e7dd3a1363b2e604fd89
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-01-10 17:28:40 +0100

    Mise à jour du code kal3iya

--- custom-addons/kal3iya_stock/models/kal3iya_stock_product.py
+++ custom-addons/kal3iya_stock/models/kal3iya_stock_product.py
@@ -4,5 +4,23 @@ class Kal3iyaStockProduct(models.Model):
     _name = 'kal3iya.stock.product'
     _description = 'Produits Stock Kal3iya'
 
-    name = fields.Char(string='Nom', required=True)
-    image_1920 = fields.Image(string='Image')
+    company_article_id = fields.Many2one(
+        'company.article',
+        string='Article Société',
+        required=True
+    )
+
+    name = fields.Char(
+        string='Nom interne Kal3iya',
+        required=True
+    )
+
+    company_article_image = fields.Image(
+        string='Image',
+        related='company_article_id.image',
+        readonly=True
+    )
+
+    _sql_constraints = [
+        ('unique_name', 'unique(name)', 'Le nom interne doit être unique.')
+    ]

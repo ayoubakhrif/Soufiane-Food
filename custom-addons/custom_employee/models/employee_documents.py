d9ee8aaf0afc54251d21109e98e03ea7ec8855c5
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-01-14 18:20:05 +0100

    Mise à jour du code kal3iya

--- custom-addons/custom_employee/models/employee_documents.py
+++ custom-addons/custom_employee/models/employee_documents.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import base64
 
 class CoreEmployeeDocument(models.Model):
     _name = 'core.employee.document'
@@ -28,7 +29,7 @@ class CoreEmployeeDocument(models.Model):
     )
     filename = fields.Char(string='Nom du fichier')
 
-    issue_date = fields.Date(string='Date du document')
+    issue_date = fields.Date(string='Date d’éxpiration')
     note = fields.Text(string='Remarque')
 
     @api.constrains('employee_id', 'doc_type')
@@ -41,3 +42,14 @@ class CoreEmployeeDocument(models.Model):
             ]
             if self.search_count(domain):
                 raise ValidationError("Un document de ce type existe déjà pour cet employé.")
+
+    @api.constrains('document_file')
+    def _check_file_size(self):
+        MAX_SIZE = 5 * 1024 * 1024  # 5 Mo
+        for rec in self:
+            if rec.document_file:
+                file_size = len(base64.b64decode(rec.document_file))
+                if file_size > MAX_SIZE:
+                    raise ValidationError(
+                        "Le fichier PDF ne doit pas dépasser 5 Mo."
+                    )

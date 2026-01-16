70448e4392e0bd1027d7668dd64864b38739ec43
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-01-16 14:40:18 +0100

    Mise à jour du code kal3iya

--- custom-addons/logistique/models/supplier.py
+++ custom-addons/logistique/models/supplier.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import re
 
 class LogistiqueSupplier(models.Model):
     _name = 'logistique.supplier'
@@ -6,3 +8,12 @@ class LogistiqueSupplier(models.Model):
 
     name = fields.Char(string='Nom', required=True)
     email = fields.Char(string='Email')
+
+
+
+    @api.constrains('email')
+    def _check_email_format(self):
+        email_regex = r'^[^@]+@[^@]+\.[^@]+$'
+        for rec in self:
+            if rec.email and not re.match(email_regex, rec.email):
+                raise ValidationError("Invalid email format.")

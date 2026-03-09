5a1500036026942b77aedee36230f821b232b30a
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-03-09 14:06:04 +0000

    Mise à jour du code kal3iya

--- custom-addons/finance/models/data_cheque.py
+++ custom-addons/finance/models/data_cheque.py
@@ -34,7 +34,7 @@ class DataCheque(models.Model):
     date_emission = fields.Date(string='Date d’émission', tracking=True)
     week = fields.Char(string='Semaine', compute='_compute_week', store=True)
     serie = fields.Char(string='Série de facture', tracking=True)
-    date_echeance = fields.Date(string='Date d’échéance', tracking=True, compute="_compute_date_echeance", store=True)
+    date_echeance = fields.Date(string='Date d’échéance', tracking=True, compute="_compute_date_echeance", store=True, readonly=False)
     date_encaissement = fields.Date(string='Date d’encaissement', tracking=True)
     ste_id = fields.Many2one('finance.ste', string='Société', tracking=True, required=True)
     benif_id = fields.Many2one('finance.benif', string='Bénificiaire', tracking=True, required=True)

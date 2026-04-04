d2b0e0635a625cc77cc2d180a38fd9b46c169d2d
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-04-04 22:13:38 +0100

    Mise à jour du code kal3iya

--- custom-addons/charges_casa/models/charges_casa.py
+++ custom-addons/charges_casa/models/charges_casa.py
@@ -36,3 +36,32 @@ class ChargesCasa(models.Model):
     def action_draft(self):
         for record in self:
             record.state = 'draft'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record in records:
+            if record.client_id:
+                record._create_client_advance()
+        return records
+
+    def write(self, vals):
+        if 'client_id' in vals:
+            old_clients = {rec.id: rec.client_id for rec in self}
+            res = super().write(vals)
+            for rec in self:
+                if rec.client_id and rec.client_id != old_clients.get(rec.id):
+                    rec._create_client_advance()
+            return res
+        return super().write(vals)
+
+    def _create_client_advance(self):
+        self.ensure_one()
+        self.env['casa.client.advance'].create({
+            'client_id': self.client_id.id,
+            'amount': self.amount,
+            'date': self.date,
+            'payment_mode': 'charge',
+            'comment': self.commentaires,
+            'state': 'draft',
+        })

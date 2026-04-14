b292ea666cc9e2db6680d119048b6f30e0d3de31
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-04-14 11:02:40 +0100

    Mise à jour du code kal3iya

---
+++ custom-addons/gestion_projet/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Gestion de Projet',
+    'version': '15.0.1.0.0',
+    'summary': 'Module de gestion de projets et de tâches associées',
+    'author': 'Your Company',
+    'category': 'Project Management',
+    'depends': ['base', 'custom_employee'],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/project_report.xml',
+        'views/project_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}

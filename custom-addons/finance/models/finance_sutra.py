5623a6da2d6378905ddf7d3e297df75cca79ead8
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-04-17 12:47:37 +0100

    Mise à jour du code kal3iya

--- custom-addons/finance/models/finance_sutra.py
+++ custom-addons/finance/models/finance_sutra.py
@@ -59,6 +59,7 @@ class FinanceSutra(models.Model):
     tva = fields.Float(string='TVA', compute='_compute_tva', store=True, tracking=True)
     
     total = fields.Float(string='Total', compute='_compute_total', store=True, tracking=True)
+    reglementation = fields.Char(string='Réglementation', tracking=True)
 
     # -------------------------------------------------------------------------
     # CHEQUE MANAGEMENT (Linked to DataCheque via Payment)

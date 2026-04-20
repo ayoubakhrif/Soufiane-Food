bf2cb78ced59943b5eb1a8ad476bc1b3bf2c2b9f
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-04-21 00:02:48 +0100

    Mise à jour du code kal3iya

--- custom-addons/stock_kal3iya/__manifest__.py
+++ custom-addons/stock_kal3iya/__manifest__.py
@@ -13,7 +13,7 @@ This module provides a robust stock management system based on an immutable move
 - Reversal moves for cancellations.
     """,
     'author': 'Soufiane-Food',
-    'depends': ['base', 'web', 'mail', 'custom_employee'],
+    'depends': ['base', 'web', 'mail', 'company_data', 'custom_employee', 'casa_stock_hanane'],
     'external_dependencies': {
         'python': ['openai'],
     },

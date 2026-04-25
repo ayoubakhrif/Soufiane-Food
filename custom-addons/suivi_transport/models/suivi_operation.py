c7c38316fd7959e51e1091389a528c4060bf92ed
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-04-25 15:52:32 +0100

    Mise à jour du code kal3iya

--- custom-addons/suivi_transport/models/suivi_operation.py
+++ custom-addons/suivi_transport/models/suivi_operation.py
@@ -20,6 +20,11 @@ class SuiviOperation(models.Model):
     montant = fields.Float(string='Montant')
     credit = fields.Float(string='Crédit', help="Si la commande n'est pas payée")
     payer_id = fields.Many2one('suivi.client', string='Qui a payé')
+    state = fields.Selection([
+        ('initial', 'Initial'),
+        ('paid', 'Payé'),
+        ('validated', 'Validé')
+    ], string='État', default='initial', tracking=True)
 
     line_ids = fields.One2many('suivi.operation.line', 'operation_id', string='Détails des opérations')
 
@@ -35,6 +40,18 @@ class SuiviOperation(models.Model):
             if rec.ville != 'casa':
                 if rec.montant <= 0 and rec.credit <= 0:
                     raise ValidationError(_("Pour les villes hors Casa, soit le montant soit le crédit doit être positif."))
+
+    def action_pay(self):
+        for rec in self:
+            if rec.credit > 0:
+                raise ValidationError(_("Impossible de passer à l'état 'Payé' tant que le crédit n'est pas nul."))
+            rec.state = 'paid'
+
+    def action_validate(self):
+        self.write({'state': 'validated'})
+
+    def action_set_initial(self):
+        self.write({'state': 'initial'})
 
 class SuiviOperationLine(models.Model):
     _name = 'suivi.operation.line'

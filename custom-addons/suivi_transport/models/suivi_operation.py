4b33059a143f4f4b727afe1419de159efd05e5c0
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-06-20 14:21:46 +0100

    Mise à jour du code

--- custom-addons/suivi_transport/models/suivi_operation.py
+++ custom-addons/suivi_transport/models/suivi_operation.py
@@ -48,9 +48,52 @@ class SuiviOperation(models.Model):
             rec.state = 'paid'
 
     def action_validate(self):
+        for rec in self:
+            for line in rec.line_ids:
+                if line.stock_id:
+                    self.env['suivi.stock.move'].create({
+                        'product_id': line.stock_id.product_id.id,
+                        'lot': line.lot,
+                        'dum': line.dum,
+                        'ville': rec.ville,
+                        'qty': -line.qty,
+                        'move_type': 'exit',
+                        'state': 'done',
+                        'date': rec.date,
+                        'reference': rec.name,
+                        'weight': line.stock_id.weight,
+                        'calibre': line.stock_id.calibre,
+                        'chauffeur_id': rec.chauffeur_id.id,
+                        'res_model': 'suivi.operation',
+                        'res_id': rec.id,
+                    })
         self.write({'state': 'validated'})
 
     def action_set_initial(self):
+        for rec in self:
+            moves = self.env['suivi.stock.move'].search([
+                ('res_model', '=', 'suivi.operation'),
+                ('res_id', '=', rec.id),
+                ('move_type', '=', 'exit'),
+                ('state', '=', 'done')
+            ])
+            for move in moves:
+                self.env['suivi.stock.move'].create({
+                    'product_id': move.product_id.id,
+                    'lot': move.lot,
+                    'dum': move.dum,
+                    'ville': move.ville,
+                    'qty': -move.qty,
+                    'move_type': 'cancel_exit',
+                    'state': 'done',
+                    'date': fields.Datetime.now(),
+                    'reference': rec.name,
+                    'weight': move.weight,
+                    'calibre': move.calibre,
+                    'chauffeur_id': rec.chauffeur_id.id,
+                    'res_model': 'suivi.operation',
+                    'res_id': rec.id,
+                })
         self.write({'state': 'initial'})
 
 class SuiviOperationLine(models.Model):
@@ -59,5 +102,34 @@ class SuiviOperationLine(models.Model):
 
     operation_id = fields.Many2one('suivi.operation', string='Opération', required=True, ondelete='cascade')
     client_id = fields.Many2one('suivi.client', string='Client')
+    
+    stock_id = fields.Many2one('suivi.stock.stock', string='Stock Disponible')
     article_id = fields.Many2one('company.article', string='Article')
+    
+    qty = fields.Float(string='Quantité', required=True, default=1.0)
     lot = fields.Char(string='LOT')
+    dum = fields.Char(string='DUM')
+
+    @api.onchange('stock_id')
+    def _onchange_stock_id(self):
+        if self.stock_id:
+            self.lot = self.stock_id.lot
+            self.dum = self.stock_id.dum
+            self.article_id = self.stock_id.product_id.article_id.id
+            if self.qty == 0.0:
+                self.qty = 1.0
+
+    @api.constrains('qty', 'stock_id')
+    def _check_stock_availability(self):
+        for line in self:
+            if line.stock_id:
+                total_ordered = sum(
+                    other_line.qty 
+                    for other_line in line.operation_id.line_ids 
+                    if other_line.stock_id.id == line.stock_id.id
+                )
+                if total_ordered > line.stock_id.quantity:
+                    raise ValidationError(_(
+                        "Quantité globale insuffisante pour le stock sélectionné.\n"
+                        "Demandée au total: %(req)s, Disponible: %(avail)s"
+                    ) % {'req': total_ordered, 'avail': line.stock_id.quantity})

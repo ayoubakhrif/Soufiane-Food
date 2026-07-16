ca7b699be8ab3c08d5158540f9f0ce3dc44e0b05
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-07-16 12:41:24 +0100

    Mise à jour du code

--- custom-addons/tresorerie_chq/models/paiement.py
+++ custom-addons/tresorerie_chq/models/paiement.py
@@ -217,7 +217,7 @@ Exemple de réponse attendue:
             "Content-Type": "application/json"
         }
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key={api_key}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={api_key}"
 
         try:
             resp = requests.post(url, headers=headers, json=payload, timeout=120)

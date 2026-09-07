16d464104d7f1b09ed5fc6628f92f092b6cd9f83
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-07 12:45:17 +0100

    Mise à jour du code

---
+++ update_manifest.py
@@ -0,0 +1,11 @@
+﻿import os
+
+manifest_path = 'custom-addons/tanger_med/__manifest__.py'
+with open(manifest_path, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("'security/security.xml',", "'security/security.xml',\n        'data/server_actions.xml',")
+
+with open(manifest_path, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Updated __manifest__.py")

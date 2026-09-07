c7e7ff964fd139fd040ac4bdc95193319f4d06de
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-07 12:52:00 +0100

    Mise à jour du code

---
+++ fix_sutra_name.py
@@ -0,0 +1,11 @@
+﻿import os
+
+filepath = 'custom-addons/tanger_med/models/sutra.py'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("vals['name'] = f'SUTRA - {log_entry.name}'", "vals['name'] = f'SUTRA - {log_entry.display_name}'")
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Updated sutra.py")

0240afae48b86e39264fad73cb694470f8e28127
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-08 16:05:46 +0100

    Ignore leading zeros when matching DUMs during SUTRA import

--- custom-addons/tanger_med/models/sutra_import_batch.py
+++ custom-addons/tanger_med/models/sutra_import_batch.py
@@ -117,7 +117,15 @@ class SutraImportLine(models.Model):
             if rec.status == 'done':
                 continue
                 
-            entry = self.env['logistique.entry'].search(['|', ('tanger_med_dum', '=', rec.dum), ('dum', '=', rec.dum)], limit=1)
+            clean_dum = rec.dum.lstrip('0')
+            domain = [
+                '|', '|', '|',
+                ('tanger_med_dum', '=', rec.dum),
+                ('dum', '=', rec.dum),
+                ('tanger_med_dum', '=', clean_dum),
+                ('dum', '=', clean_dum)
+            ]
+            entry = self.env['logistique.entry'].search(domain, limit=1)
             if entry:
                 rec.logistics_id = entry.id
                 rec.status = 'ready'

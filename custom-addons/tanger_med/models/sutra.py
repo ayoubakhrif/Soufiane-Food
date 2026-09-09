4f96b69654bd89bd1a1be0e319333d56871a6f48
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 14:07:52 +0100

    Add dum and payment_state to sutra dossier

--- custom-addons/tanger_med/models/sutra.py
+++ custom-addons/tanger_med/models/sutra.py
@@ -47,6 +47,27 @@ class SutraDossier(models.Model):
     amount = fields.Float(string='Montant SUTRA', tracking=True)
     facture_ids = fields.One2many('sutra.facture', 'sutra_id', string='Factures')
 
+    dum = fields.Char(string='DUM', compute='_compute_dum', store=True)
+    payment_state = fields.Selection([
+        ('non_paye', 'Non Paye'),
+        ('paye', 'Paye')
+    ], string='Etat de Paiement', compute='_compute_payment_state', store=True)
+
+    @api.depends('logistics_id', 'logistics_id.dum', 'logistics_id.tanger_med_dum')
+    def _compute_dum(self):
+        for rec in self:
+            rec.dum = rec.logistics_id.tanger_med_dum or rec.logistics_id.dum or ''
+
+    @api.depends('facture_ids', 'facture_ids.state')
+    def _compute_payment_state(self):
+        for rec in self:
+            if not rec.facture_ids:
+                rec.payment_state = 'non_paye'
+            elif all(f.state == 'paye' for f in rec.facture_ids):
+                rec.payment_state = 'paye'
+            else:
+                rec.payment_state = 'non_paye'
+
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:

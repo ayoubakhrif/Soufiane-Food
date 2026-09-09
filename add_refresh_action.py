533dd706e103cc246fae1a449ec12dbdb7ffdbeb
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 14:41:42 +0100

    Mise à jour du code

---
+++ add_refresh_action.py
@@ -0,0 +1,28 @@
+﻿import os
+
+filepath = 'custom-addons/tanger_med/data/server_actions.xml'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+new_action = """
+    <!-- Server Action to Refresh DUM on old SUTRA Dossiers -->
+    <record id="action_refresh_sutra_dossier_dum" model="ir.actions.server">
+        <field name="name">Rafraichir les DUM</field>
+        <field name="model_id" ref="model_sutra_dossier"/>
+        <field name="binding_model_id" ref="model_sutra_dossier"/>
+        <field name="binding_view_types">list</field>
+        <field name="state">code</field>
+        <field name="code">
+            if records:
+                for rec in records:
+                    rec._compute_dum()
+        </field>
+    </record>
+
+</data>"""
+
+content = content.replace('</data>', new_action)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Added server action")

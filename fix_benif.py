de5d36928f49a6042c8d7ff2f5c975b36b97c658
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 17:17:29 +0100

    Mise à jour du code

---
+++ fix_benif.py
@@ -0,0 +1,11 @@
+﻿import os
+
+filepath = 'custom-addons/finance_2/controllers/whatsapp_finance_api.py'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("rep.benif_id.name if rep.benif_id else ''", "c_v2.benif_id.name if c_v2.benif_id else ''")
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Fixed rep.benif_id")
---
+++ fix_rep_state.py
@@ -0,0 +1,11 @@
+﻿import os
+
+filepath = 'custom-addons/finance_2/controllers/whatsapp_finance_api.py'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("dict(rep._fields['state'].selection).get(rep.state) or rep.state", "'-'")
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Fixed rep.state")

f19ba1a6b15ea8211cf56b21d6681bd000162eda
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 12:07:09 +0100

    Mise à jour du code

---
+++ fix_view_attrs.py
@@ -0,0 +1,22 @@
+﻿import os
+
+filepath = 'custom-addons/tanger_med/views/sutra_views.xml'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Fix form view
+content = content.replace(
+    '<field name="amount_total_debt" widget="monetary" style="color: #dc3545;"/>',
+    '<field name="amount_total_debt" widget="monetary" class="text-danger"/>'
+)
+
+# Fix tree view
+content = content.replace(
+    '<field name="amount_total_debt" sum="Total Dette SUTRA" style="font-weight: bold;"/>',
+    '<field name="amount_total_debt" sum="Total Dette SUTRA" decoration-bf="1" decoration-danger="1"/>'
+)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("Fixed view attributes")

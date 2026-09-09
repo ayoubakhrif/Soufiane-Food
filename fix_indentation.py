85e2f7bc16f23e4c38e1e705b428cb784c1c04ba
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 15:38:43 +0100

    Mise à jour du code

---
+++ fix_indentation.py
@@ -0,0 +1,47 @@
+﻿import os
+
+filepath = 'custom-addons/finance_2/models/cheque.py'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+bad_block = """def action_encaisser(self):
+        for rec in self:
+            if rec.state != 'cloture':
+                raise UserError("Seuls les chèques clôturés peuvent être encaissés.")
+            if not rec.date_encaissement:
+                raise UserError("Veuillez renseigner la date d'encaissement.")
+            if not rec.montant_encaisse:
+                raise UserError("Veuillez renseigner le montant encaissé.")
+                
+            rec.state = 'encaisse'
+
+    def action_annuler_encaissement(self):
+        for rec in self:
+            if rec.state == 'encaisse':
+                rec.state = 'cloture'
+                rec.date_encaissement = False
+                rec.montant_encaisse = 0.0"""
+
+good_block = """    def action_encaisser(self):
+        for rec in self:
+            if rec.state != 'cloture':
+                raise UserError("Seuls les chèques clôturés peuvent être encaissés.")
+            if not rec.date_encaissement:
+                raise UserError("Veuillez renseigner la date d'encaissement.")
+            if not rec.montant_encaisse:
+                raise UserError("Veuillez renseigner le montant encaissé.")
+                
+            rec.state = 'encaisse'
+
+    def action_annuler_encaissement(self):
+        for rec in self:
+            if rec.state == 'encaisse':
+                rec.state = 'cloture'
+                rec.date_encaissement = False
+                rec.montant_encaisse = 0.0"""
+
+content = content.replace(bad_block, good_block)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Fixed indentation")

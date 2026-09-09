2bb970b9a8f053cd23a50e07d9a2f3d3520e8e3c
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 15:36:19 +0100

    Mise à jour du code

---
+++ remove_action.py
@@ -0,0 +1,15 @@
+﻿import os
+import re
+
+filepath = 'custom-addons/tanger_med/data/server_actions.xml'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Pattern to remove the specific server action block
+pattern = r'\s*<!-- Server Action to Refresh DUM on old SUTRA Dossiers -->\s*<record id="action_refresh_sutra_dossier_dum" model="ir\.actions\.server">.*?</record>'
+
+new_content = re.sub(pattern, '', content, flags=re.DOTALL)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(new_content)
+print("Removed server action")
---
+++ update_cheque_view.py
@@ -0,0 +1,21 @@
+﻿import os
+import re
+
+filepath = 'custom-addons/finance_2/views/cheque_views.xml'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Add button in Encaissement tree
+old_tree_btn = '<button name="action_encaisser" type="object" icon="fa-check" string="Encaisser" class="oe_highlight" invisible="state != \'cloture\'"/>'
+new_tree_btn = """<button name="action_encaisser" type="object" icon="fa-check" string="Encaisser" class="oe_highlight" invisible="state != 'cloture'"/>
+                <button name="action_annuler_encaissement" type="object" icon="fa-undo" string="Annuler Encaissement" class="btn-danger" invisible="state != 'encaisse'"/>"""
+content = content.replace(old_tree_btn, new_tree_btn)
+
+# Modify Action Context for Encaissement
+old_action_context = "<field name=\"context\">{'search_default_cloture': 1}</field>"
+new_action_context = "<field name=\"context\">{'search_default_cloture': 1, 'group_by': ['date_encaissement:day']}</field>"
+content = content.replace(old_action_context, new_action_context)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Updated XML view")

0a3bd29edf432b4ab6e30b8fdedf31c25a8ab0d5
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-09 16:14:13 +0100

    Hide SUTRA Factures page in Cheque if beneficiary is not SUTRA

--- custom-addons/tanger_med/models/finance2_cheque_inherit.py
+++ custom-addons/tanger_med/models/finance2_cheque_inherit.py
@@ -4,6 +4,13 @@ class Finance2Cheque(models.Model):
     _inherit = 'finance2.cheque'
 
     sutra_facture_ids = fields.One2many('sutra.facture', 'cheque_id', string='Factures SUTRA liees')
+    is_sutra_benif = fields.Boolean(compute='_compute_is_sutra_benif')
+
+    @api.depends('benif_id', 'benif_id.name')
+    def _compute_is_sutra_benif(self):
+        for rec in self:
+            rec.is_sutra_benif = bool(rec.benif_id and rec.benif_id.name and 'sutra' in rec.benif_id.name.lower())
+
 
     def write(self, vals):
         res = super(Finance2Cheque, self).write(vals)

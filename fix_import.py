3ca8662e06505bf14e652308212cecb6c4f1653e
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-11 12:00:12 +0100

    Mise à jour du code

---
+++ fix_import.py
@@ -0,0 +1,11 @@
+﻿import os
+
+filepath = 'custom-addons/tanger_med/models/sutra.py'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('from odoo.exceptions import ValidationError, exceptions', 'from odoo.exceptions import ValidationError, UserError')
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Fixed import error")

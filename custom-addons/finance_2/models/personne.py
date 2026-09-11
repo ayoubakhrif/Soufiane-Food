0c30e0365eb0426aff110f1b2156d0eefad635d2
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-11 16:24:48 +0100

    Add total credit/encaisse to beneficiary and week to search

--- custom-addons/finance_2/models/personne.py
+++ custom-addons/finance_2/models/personne.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Finance2Personne(models.Model):
     _name = 'finance2.personne'
@@ -24,3 +24,17 @@ class Finance2Benif(models.Model):
     name = fields.Char(string='Nom du bénéficiaire', required=True)
     active = fields.Boolean(default=True)
     cheque_ids = fields.One2many('finance2.cheque', 'benif_id', string='Chèques')
+
+    total_credit = fields.Float(string='Total Crédit', compute='_compute_totals')
+    total_encaisse = fields.Float(string='Total Encaissé', compute='_compute_totals')
+    solde = fields.Float(string='Solde à ce jour', compute='_compute_totals')
+
+    @api.depends('cheque_ids', 'cheque_ids.amount_total', 'cheque_ids.montant_encaisse', 'cheque_ids.state')
+    def _compute_totals(self):
+        for rec in self:
+            credit = sum(c.amount_total for c in rec.cheque_ids if c.state != 'annule')
+            encaisse = sum(c.montant_encaisse or c.amount_total for c in rec.cheque_ids if c.state == 'encaisse')
+            rec.total_credit = credit
+            rec.total_encaisse = encaisse
+            rec.solde = credit - encaisse
+

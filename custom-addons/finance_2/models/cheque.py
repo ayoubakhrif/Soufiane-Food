85efa04a19aec63d8a67cee738cf98a05fe15c9f
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-18 15:32:23 +0100

    fix: remove duplicate sutra_facture_ids from finance_2 cheque (already in tanger_med inherit)

--- custom-addons/finance_2/models/cheque.py
+++ custom-addons/finance_2/models/cheque.py
@@ -118,8 +118,6 @@ class Finance2Cheque(models.Model):
     # Répartitions
     repartition_ids = fields.One2many('finance2.repartition', 'cheque_id', string='Répartitions')
 
-    # Factures SUTRA liées à ce chèque
-    sutra_facture_ids = fields.One2many('sutra.facture', 'cheque_id', string='Factures SUTRA')
 
 
     @api.model_create_multi

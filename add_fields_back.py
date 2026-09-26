9e8018ee110b0cfcc7040dc9bba9f675435a9101
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-26 16:18:33 +0000

    Mise à jour du code

---
+++ add_fields_back.py
@@ -0,0 +1,19 @@
+﻿import codecs
+
+with codecs.open('custom-addons/kal3iya_stock/views/kal3iya_stock_driver_views.xml', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Add phone to tree
+content = content.replace(
+    '                <field name="employee_id"/>\n            </tree>',
+    '                <field name="employee_id"/>\n                <field name="phone"/>\n            </tree>'
+)
+
+# Add phone and password to form
+content = content.replace(
+    '                            <field name="employee_id"/>\n                        </group>',
+    '                            <field name="employee_id"/>\n                            <field name="phone"/>\n                            <field name="password" password="True"/>\n                        </group>'
+)
+
+with codecs.open('custom-addons/kal3iya_stock/views/kal3iya_stock_driver_views.xml', 'w', encoding='utf-8') as f:
+    f.write(content)

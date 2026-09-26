a20592ad825b761c8cfed54fbf0e78f71a56420b
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-26 16:36:23 +0000

    Mise à jour du code

---
+++ fix_init_order.py
@@ -0,0 +1,18 @@
+﻿import codecs
+import re
+
+filepath = 'custom-addons/stock_casa_field/models/casa_field_stock_stock_stock.py'
+with codecs.open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+new_content = content.replace(
+    'def init(self):\n        tools.drop_view_if_exists(self.env.cr, self._table)',
+    'def init(self):\n        tools.drop_view_if_exists(self.env.cr, self._table)\n        if not odoo.tools.table_exists(self.env.cr, \"casa_field_stock_move\"):\n            self.env[\"casa_field.stock.move\"]._auto_init()'
+)
+
+# We need to import odoo at the top if it's not there
+if 'import odoo' not in new_content:
+    new_content = 'import odoo\n' + new_content
+
+with codecs.open(filepath, 'w', encoding='utf-8') as f:
+    f.write(new_content)
---
+++ fix_others.py
@@ -0,0 +1,23 @@
+﻿import codecs
+import os
+
+files_to_fix = [
+    ('custom-addons/kal3iya_stock/models/kal3iya_stock_stock_stock.py', 'kal3iya_stock_move', 'kal3iya.stock.move'),
+    ('custom-addons/stock_kal3iya/models/stock_kal3iya_stock_stock.py', 'stock_kal3iya_move', 'stock.kal3iya.move')
+]
+
+for filepath, table_name, model_name in files_to_fix:
+    if os.path.exists(filepath):
+        with codecs.open(filepath, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        
+        new_lines = []
+        for line in lines:
+            new_lines.append(line)
+            if 'def init(self):' in line:
+                new_lines.append('        import odoo\n')
+                new_lines.append(f'        if not odoo.tools.table_exists(self.env.cr, "{table_name}"):\n')
+                new_lines.append(f'            self.env["{model_name}"]._auto_init()\n')
+        
+        with codecs.open(filepath, 'w', encoding='utf-8') as f:
+            f.write(''.join(new_lines))

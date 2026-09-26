af1541fbbbdd14f658c6f9921a5bb7425a7df5b8
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-26 16:22:40 +0000

    Mise à jour du code

---
+++ add_to_casa.py
@@ -0,0 +1,19 @@
+﻿import codecs
+
+with codecs.open('custom-addons/stock_casa_field/models/kal3iya_stock_driver.py', 'w', encoding='utf-8') as f:
+    f.write('''from odoo import models, fields
+
+class Kal3iyaStockDriver(models.Model):
+    _name = 'kal3iya.stock.driver'
+    _description = 'Chauffeurs Stock Kal3iya'
+
+    name = fields.Char(string='Nom', required=True)
+    employee_id = fields.Many2one(
+        'core.employee', 
+        string='EmployÃ©', 
+        domain="[('job_position_id.name', 'ilike', 'Chauffeur')]",
+        help="Linked HR Employee. Filtered by job position 'Chauffeur'."
+    )
+    phone = fields.Char(string='Telephone')
+    password = fields.Char(string='Mot de passe')
+''')
---
+++ fix_casa.py
@@ -0,0 +1,9 @@
+﻿import codecs
+
+with codecs.open('custom-addons/stock_casa_field/models/kal3iya_stock_driver.py', 'w', encoding='utf-8') as f:
+    f.write('''from odoo import models, fields
+
+class Kal3iyaStockDriverCasa(models.Model):
+    _inherit = 'kal3iya.stock.driver'
+
+''')

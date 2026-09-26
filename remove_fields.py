8e6a73c9890e9cdb417827d938800259f976a126
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-26 16:16:16 +0000

    Mise à jour du code

---
+++ remove_fields.py
@@ -0,0 +1,10 @@
+﻿import codecs
+
+with codecs.open('custom-addons/kal3iya_stock/views/kal3iya_stock_driver_views.xml', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('<field name="phone"/>\n', '')
+content = content.replace('                            <field name="password" password="True"/>\n', '')
+
+with codecs.open('custom-addons/kal3iya_stock/views/kal3iya_stock_driver_views.xml', 'w', encoding='utf-8') as f:
+    f.write(content)

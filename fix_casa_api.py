eecf2a2e0f7445f7101049afa65d60ae1a96c4d2
Ayoub Akhrif <ayoubakhrif03@gmail.com>
2026-09-26 16:28:35 +0000

    Mise à jour du code

---
+++ fix_casa_api.py
@@ -0,0 +1,35 @@
+﻿import codecs
+
+with codecs.open('custom-addons/stock_casa_field/controllers/api_stock.py', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+old_bootstrap = '''        clients = request.env['kal3iya.stock.client'].sudo().search_read(
+            [], ['id', 'name']
+        )
+
+        return self._json_response({
+            'status': 'success',
+            'products': products,
+            'clients': clients,
+            'garages': GARAGE_SELECTION,
+        })'''
+
+new_bootstrap = '''        clients = request.env['kal3iya.stock.client'].sudo().search_read(
+            [], ['id', 'name']
+        )
+        drivers = request.env['kal3iya.stock.driver'].sudo().search_read(
+            [], ['id', 'name']
+        )
+
+        return self._json_response({
+            'status': 'success',
+            'products': products,
+            'clients': clients,
+            'drivers': drivers,
+            'garages': GARAGE_SELECTION,
+        })'''
+
+content = content.replace(old_bootstrap, new_bootstrap)
+
+with codecs.open('custom-addons/stock_casa_field/controllers/api_stock.py', 'w', encoding='utf-8') as f:
+    f.write(content)
